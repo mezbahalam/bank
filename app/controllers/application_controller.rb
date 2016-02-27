7b94cfb2bd967b8f76662b9e0b2623b40ac47e5a
mezbahalam <mezbahalam26@gmail.com>
2016-02-27 23:26:57 +0600

    name in app_contrl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:birthday, :account, :email, :password) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:name, :birthday, :account, :email, :password) }
   end
 end
