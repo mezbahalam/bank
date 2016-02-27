0ceb77d7722d211ca49018e81a11115e561c0150
mezbahalam <mezbahalam26@gmail.com>
2016-02-27 21:34:54 +0600

    adsss

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,4 @@
-class Customer < ActiveRecord::Base
+class Customer  < ActiveRecord::Base
   devise :database_authenticatable, :trackable, :validatable, :authentication_keys => [:account, :birthday]
 
   def email_required?
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
-  devise_for :customers, :controllers => { :registrations => "registrations",sessions: 'customers/sessions' }
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  devise_for :customers, :controllers => { :registrations => "registrations", sessions: 'customers/sessions' }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
